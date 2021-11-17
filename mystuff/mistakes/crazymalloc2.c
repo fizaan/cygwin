600c595591e844539097da7148480b7237f60895
Faiz <fizaan@gmail.com>
2021-11-16 19:11:01 -0500

    Updated

--- mystuff/mistakes/crazymalloc2.c
+++ mystuff/mistakes/crazymalloc2.c
@@ -3,9 +3,18 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-// cd /cygdrive/c/Users/Alifa/Desktop/cygwin64-workspace/mystuff/mistakes/
-// gcc -o out.bin crazymalloc2.c -Wall -Wextra -pedantic
-// I asked this question on stackoverflow.com/questions/69977548
+/*
+    cd /cygdrive/c/Users/Alifa/Desktop/cygwin64-workspace/mystuff/mistakes/
+    gcc -o out.bin crazymalloc2.c -Wall -Wextra -pedantic
+    
+    I asked this question on stackoverflow.com/questions/69977548
+    How does one align a pointer that is allocated on the heap (using malloc)?
+
+    ---
+    0x6fffffef0010
+    0x6fffffef1000
+    1be9f82c
+*/
 
 #define BY2PG 0x1000
 char * vm;
@@ -14,8 +23,9 @@ char * vm;
 static uintptr_t roundup(uintptr_t n, int align) {
     if(n % align == 0) ; 
     // is already aligned so
-    // no action required, else
-    // align it:
+    // no action required
+
+    // else, align it:
     else n += (align - (n % align));
     
     return n;
@@ -25,13 +35,13 @@ int main() {
 
     vm = (char *) calloc(0x100000, sizeof(char));
 
-    printf("%08x\n", vm);
+    printf("%p\n", (void *) vm);
 
-    vm = (char *) roundup((uintptr_t)vm, BY2PG);
+    u_int * intp = (u_int *) roundup((uintptr_t)vm, BY2PG);
 
-    printf("%08x\n", vm);
+    printf("%p\n", (void *) intp);
 
-    vm[0] = 5;
+    intp[0] = 0x1be9f82c;
 
-    printf("%d\n", vm[0]);
+    printf("%08x\n", intp[0]);
 }
